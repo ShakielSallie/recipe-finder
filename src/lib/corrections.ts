b134af486f8e683ba19cba1e3bf42f8dbcd5e82e
Shakiel Sallie <shakiels@interfile.co.za>
2026-06-30 14:44:53 +0200

    Fix duplicate keys in corrections dictionary

--- src/lib/corrections.ts
+++ src/lib/corrections.ts
@@ -1,39 +1,49 @@
 const corrections: Record<string, string> = {
   // Common English misspellings
   teh: 'the', adn: 'and', hte: 'the', taht: 'that', waht: 'what', wiht: 'with',
-  hwo: 'how', nad: 'and', yuo: 'you', hav: 'have', recieve: 'receive',
-  beleive: 'believe', freind: 'friend', wierd: 'weird', thier: 'their',
-  occured: 'occurred', occurance: 'occurrence', begining: 'beginning',
-  seperate: 'separate', definately: 'definitely', probaly: 'probably',
-  probabaly: 'probably', prolly: 'probably', becuase: 'because',
-  becasue: 'because', untill: 'until', tommorrow: 'tomorrow', tommorow: 'tomorrow',
-  tomarrow: 'tomorrow', writting: 'writing', writen: 'written', truely: 'truly',
-  arguement: 'argument', concious: 'conscious', suprise: 'surprise',
-  grammer: 'grammar', sieze: 'seize', neccessary: 'necessary',
-  necesary: 'necessary', necesarry: 'necessary', sucessful: 'successful',
+  hwo: 'how', nad: 'and', yuo: 'you', recieve: 'receive', beleive: 'believe',
+  freind: 'friend', wierd: 'weird', thier: 'their', occured: 'occurred',
+  begining: 'beginning', seperate: 'separate', definately: 'definitely',
+  probaly: 'probably', probabaly: 'probably', becuase: 'because',
+  becasue: 'because', untill: 'until', tommorrow: 'tomorrow', tomarrow: 'tomorrow',
+  writting: 'writing', writen: 'written', truely: 'truly', arguement: 'argument',
+  concious: 'conscious', suprise: 'surprise', grammer: 'grammar', sieze: 'seize',
+  neccessary: 'necessary', necesary: 'necessary', necesarry: 'necessary',
   succesful: 'successful', recomend: 'recommend', reccomend: 'recommend',
-  reccomend: 'recommend', refered: 'referred', prefered: 'preferred',
-  occassion: 'occasion', relevent: 'relevant', independant: 'independent',
-  existance: 'existence', knowlege: 'knowledge', accomodate: 'accommodate',
-  enviroment: 'environment', unfortunatly: 'unfortunately', immedietly: 'immediately',
-  immediatly: 'immediately', basicaly: 'basically', finaly: 'finally',
-  usefull: 'useful', carefull: 'careful', gratefull: 'grateful',
-  hopefull: 'hopeful', helpfull: 'helpful', beautifull: 'beautiful',
+  refered: 'referred', prefered: 'preferred', occassion: 'occasion',
+  relevent: 'relevant', independant: 'independent', existance: 'existence',
+  knowlege: 'knowledge', accomodate: 'accommodate', enviroment: 'environment',
+  unfortunatly: 'unfortunately', immedietly: 'immediately', immediatly: 'immediately',
+  basicaly: 'basically', finaly: 'finally', usefull: 'useful', carefull: 'careful',
+  gratefull: 'grateful', hopefull: 'hopeful', helpfull: 'helpful',
+  beautifull: 'beautiful',
 
-  // Food & recipe misspellings
+  // Recipe
   recipie: 'recipe', reciepe: 'recipe', recepie: 'recipe', reicpe: 'recipe',
   recipee: 'recipe', recpie: 'recipe',
+
+  // Chicken
   chiken: 'chicken', chikken: 'chicken', chiecken: 'chicken', chciken: 'chicken',
+
+  // Spaghetti
   spagetti: 'spaghetti', spagehtti: 'spaghetti', spghetti: 'spaghetti',
   spageti: 'spaghetti', spaghetii: 'spaghetti',
+
+  // Herbs & veg
   parsly: 'parsley', parsely: 'parsley', parseley: 'parsley',
-  brocolli: 'broccoli', brocoli: 'broccoli', brocolli: 'broccoli',
+  brocolli: 'broccoli', brocoli: 'broccoli',
   zuchini: 'zucchini', zucchinni: 'zucchini', zuchhinni: 'zucchini',
   avacado: 'avocado', avocardo: 'avocado', avacardo: 'avocado',
   cinnimon: 'cinnamon', cinammon: 'cinnamon', cinnamin: 'cinnamon',
   garlick: 'garlic', galic: 'garlic',
   onoin: 'onion', oinon: 'onion',
   tomatoe: 'tomato', tomate: 'tomato', potatoe: 'potato',
+  coliflower: 'cauliflower', califlower: 'cauliflower', caulflower: 'cauliflower',
+  mashrooms: 'mushrooms', mushroms: 'mushrooms',
+  corriander: 'coriander', coriender: 'coriander',
+  tumeric: 'turmeric',
+
+  // Dishes
   carbonarra: 'carbonara', carbonnara: 'carbonara', carbanara: 'carbonara',
   fetucini: 'fettuccine', fettucini: 'fettuccine', fetuccini: 'fettuccine',
   lasange: 'lasagne', lasagnia: 'lasagna', lasanga: 'lasagna',
@@ -43,66 +53,42 @@ const corrections: Record<string, string> = {
   parmesian: 'parmesan', parmasan: 'parmesan', parmeson: 'parmesan',
   caeser: 'caesar', ceasar: 'caesar',
   teriyakie: 'teriyaki', teriyake: 'teriyaki',
-  coliflower: 'cauliflower', califlower: 'cauliflower', caulflower: 'cauliflower',
-  mashrooms: 'mushrooms', mushroms: 'mushrooms', mashroom: 'mushroom',
   vinegerette: 'vinaigrette', vinagerette: 'vinaigrette', vinagrette: 'vinaigrette',
   burito: 'burrito', burritto: 'burrito',
   quesadila: 'quesadilla', quesedilla: 'quesadilla',
-  enchilada: 'enchilada', enchillada: 'enchilada',
-  guacamol: 'guacamole', guacamole: 'guacamole',
-  jalapeno: 'jalapeño', jalapenos: 'jalapeños',
-  tzatzki: 'tzatziki', tzatzki: 'tzatziki',
+  enchillada: 'enchilada',
+  tzatzki: 'tzatziki',
   hummous: 'hummus', humus: 'hummus', humas: 'hummus',
   tabouleh: 'tabbouleh', tabbouli: 'tabbouleh',
-  falafel: 'falafel', felafel: 'falafel',
+  felafel: 'falafel',
   mousaka: 'moussaka', musaka: 'moussaka',
   mayonaise: 'mayonnaise', mayanaise: 'mayonnaise', mayonnase: 'mayonnaise',
-  worcestershire: 'worcestershire', worchester: 'worcestershire',
-  tumeric: 'turmeric', tumeric: 'turmeric',
-  corriander: 'coriander', coriender: 'coriander',
   margerine: 'margarine', margaraine: 'margarine',
-  butterscoch: 'butterscotch', butterscotch: 'butterscotch',
-  refridgerate: 'refrigerate', refridgerator: 'refrigerator',
-  marinaide: 'marinade', marinade: 'marinade',
-  saute: 'sauté', sutee: 'sauté',
-  flambe: 'flambé',
-  creme: 'crème', creame: 'cream',
   biscut: 'biscuit', bisquit: 'biscuit',
   sandwhich: 'sandwich', sandwitch: 'sandwich',
-  burgur: 'burger', burguers: 'burgers',
-  deserts: 'desserts', dessert: 'dessert', desssert: 'dessert',
-  chocolat: 'chocolate', choclate: 'chocolate', chocloate: 'chocolate',
+  burgur: 'burger',
+  deserts: 'desserts', desssert: 'dessert',
+  choclate: 'chocolate', chocloate: 'chocolate',
   vanila: 'vanilla', vanilia: 'vanilla',
-  straberry: 'strawberry', strawbery: 'strawberry', strawberry: 'strawberry',
-  bluberry: 'blueberry', blueberry: 'blueberry',
+  straberry: 'strawberry', strawbery: 'strawberry',
   rasberry: 'raspberry', rasbperry: 'raspberry',
-  cranberry: 'cranberry', cranbery: 'cranberry',
+  cranbery: 'cranberry',
+  refridgerate: 'refrigerate', refridgerator: 'refrigerator',
+  marinaide: 'marinade',
 
-  // Common cooking actions
-  frie: 'fry', freid: 'fried', fied: 'fried',
-  boild: 'boiled', biol: 'boil',
-  grild: 'grilled', gril: 'grill',
-  rosted: 'roasted', rost: 'roast',
-  stemed: 'steamed', steem: 'steam',
-  baked: 'baked', bake: 'bake',
-  miced: 'minced', mince: 'mince',
-  choped: 'chopped', chop: 'chop',
-  diced: 'diced', dice: 'dice',
-  slised: 'sliced', slise: 'slice',
-  blended: 'blended', blened: 'blended',
-  simered: 'simmered', simmer: 'simmer',
-  braised: 'braised', brased: 'braised',
+  // Cooking verbs
+  fied: 'fried', boild: 'boiled', grild: 'grilled', rosted: 'roasted',
+  stemed: 'steamed', miced: 'minced', choped: 'chopped', slised: 'sliced',
+  simered: 'simmered', brased: 'braised',
   carmelise: 'caramelise', caramalise: 'caramelise', carmelized: 'caramelized',
 };
 
 export function autocorrectWord(word: string): string | null {
   const lower = word.toLowerCase();
-  if (corrections[lower]) {
-    const isCapitalized = word[0] >= 'A' && word[0] <= 'Z';
-    const corrected = corrections[lower];
-    return isCapitalized
-      ? corrected.charAt(0).toUpperCase() + corrected.slice(1)
-      : corrected;
-  }
-  return null;
+  if (!corrections[lower]) return null;
+  const corrected = corrections[lower];
+  const isCapitalized = word[0] >= 'A' && word[0] <= 'Z';
+  return isCapitalized
+    ? corrected.charAt(0).toUpperCase() + corrected.slice(1)
+    : corrected;
 }
